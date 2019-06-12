ec0745861ca08988e74d93df8d188551dcdd0b06
mac-zsr <18630148889@163.com>
2019-06-12 08:10:46 +0800

    logh

---
+++ src/main/scala/com/zsr/dmp/tools/utils/String2IntOrDouble.scala
@@ -0,0 +1,24 @@
+package com.zsr.dmp.tools.utils
+
+/**
+  * @author: zhouyi
+  * @create: 2019-06-11 22:58:46
+  **/
+object String2IntOrDouble {
+  // 转换Int
+  def toInt(str:String):Int={
+    try{
+      str.toInt
+    }catch {
+      case _:Exception =>0
+    }
+  }
+  // 转换Double
+  def toDouble(str:String):Double={
+    try{
+      str.toDouble
+    }catch {
+      case _:Exception =>0
+    }
+  }
+}
